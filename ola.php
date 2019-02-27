c7626303fee7854a79f64f81f8b617ee0e7ffa7f
SrDlopes <antenorl69@gmail.com>
2019-02-27 08:45:40 -0500

    Enviando meu Primeiro Arquivo atravez do git

---
+++ ola.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Testando PHP</title>
+	<style type="text/css">
+		h2 {
+			color: deepskyblue;
+			text-shadow: 1px 1px 1px black;
+		}
+	</style>
+</head>
+<body>
+	<h1>Testando PHP</h1>
+	<?php 
+		echo "<h2> Olá, <br/> Mundo!</h2>";
+	 ?>
+
+</body>
+</html>
